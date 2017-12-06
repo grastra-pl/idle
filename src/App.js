0d79ac73e34b93865016aa53b9813bc96aca49a9
Tymoteusz <tymoteusz@L01012718.local>
2017-12-06 13:31:14 +0100

    kroczek początkowy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class ZmianaZasobow {
+  constructor(jakich, oile) {
+    this.jakich = jakich;
+    this.oile = oile;
+  }
+}
 
 class App extends Component {
   render() {
+    let a = new ZmianaZasobow("jakowych","iletam");
+
+    console.log(a);
+
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
